d254c610f05beb357cf4fd9f5b65c9cf9bd99723
Juan Carlos Cancela <cancela.juancarlos@gmail.com>
2015-05-01 19:10:55 -0300

    added operation object | refactor

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  * @type {Function}
  */
 module.exports = {
-    Machine: require('./lib/Machine').Machine,
-    Transition: require('./lib/Transition').Transition,
-    StateTransitionExpression : require('./lib/StateTransitionExpression').StateTransitionExpression
+    Machine                   : require('./lib/Machine').Machine,
+    Transition                : require('./lib/Transition').Transition,
+    StateTransitionExpression : require('./lib/StateTransitionExpression').StateTransitionExpression,
+    Operation                 : require('./lib/Operation').Operation
 };
